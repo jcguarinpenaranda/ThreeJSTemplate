e7f04cb5056e1bf668c08c5371a76d3dd77ebf8d
Juan Camilo Guarin Peñaranda <jcguarinpenaranda@gmail.com>
2015-01-15 17:31:10 -0500

    Added Three js and made the template

---
+++ js/app.js
@@ -0,0 +1,32 @@
+(function(){
+
+	var scene, camera, renderer;
+
+    init();
+    animate();
+
+    //Use this for initialization
+    function init() {
+        scene = new THREE.Scene();
+
+        camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 1, 10000 );
+        camera.position.z = 1000;
+
+
+        renderer = new THREE.WebGLRenderer();
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+        document.body.appendChild( renderer.domElement );
+    }
+
+    //This is called each frame
+    function animate() {
+
+        requestAnimationFrame( animate );
+
+        //your code here...
+
+        renderer.render( scene, camera );
+    }
+
+})();
